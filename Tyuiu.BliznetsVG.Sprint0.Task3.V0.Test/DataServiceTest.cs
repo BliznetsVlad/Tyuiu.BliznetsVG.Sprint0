6bdab25285b051f818c9da8aae2a9a1356335d93
legen <legen@VIXSIDE>
2026-09-17 01:21:49 +0500

    Task2

---
+++ Tyuiu.BliznetsVG.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BliznetsVG.Sprint0.Task3.V0.Lib;
+
+
+
+namespace Tyuiu.BliznetsVG.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
---
+++ Tyuiu.BliznetsVG.Sprint0.Task3.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BliznetsVG.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BliznetsVG.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ef958754-dbd6-4351-8d8c-4ee9a6bc356a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
