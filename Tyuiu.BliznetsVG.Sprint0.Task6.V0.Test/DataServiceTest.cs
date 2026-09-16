191a0d691aef3720a77be376765cc5e1ffdfbcbc
legen <legen@VIXSIDE>
2026-09-17 03:57:01 +0500

    Task7

--- Tyuiu.BliznetsVG.Sprint0.Task6.V0.Test/DataServiceTest.cs
+++ Tyuiu.BliznetsVG.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -4,32 +4,32 @@ using Tyuiu.BliznetsVG.Sprint0.Task6.V0.Lib;
 
 namespace Tyuiu.BliznetsVG.Sprint0.Task6.V0.Test
 {
-        [TestClass]
-        public class DataServiceTest
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArrayValid()
         {
-            [TestMethod]
-            public void CheckAdditionArrayValid()
-            {
-                var numbers = new int[] { 1, 2, 3, 4, 5 };
-                var res = DataService.AdditionArray(numbers);
-                Assert.AreEqual(15, res);
-            }
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(numbers);
+            Assert.AreEqual(15, res);
+        }
 
-            [TestMethod]
-            public void CheckSubtractionArrayValid()
-            {
-                var numbers = new int[] { 1, 2, 3, 4, 5 };
-                var res = DataService.SubtractionArray(numbers);
-                Assert.AreEqual(-15, res);
-            }
+        [TestMethod]
+        public void CheckSubtractionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubtractionArray(numbers);
+            Assert.AreEqual(-15, res);
+        }
 
-            [TestMethod]
-            public void CheckMultArrayValid()
-            {
-                var numbers = new int[] { 1, 2, 3, 4, 5 };
-                var res = DataService.MultiplicationArray(numbers);
-                Assert.AreEqual(120, res);
-            }
+        [TestMethod]
+        public void CheckMultArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplicationArray(numbers);
+            Assert.AreEqual(120, res);
         }
     }
+
 }
---
+++ Tyuiu.BliznetsVG.Sprint0.Task7.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BliznetsVG.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BliznetsVG.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8d086836-21e5-48f2-8fe2-26c52cbe772b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
