a84ba6fcc757d86bb362fb31b97f9175d2ee6479
legen <legen@VIXSIDE>
2026-09-17 02:31:41 +0500

    Task4

--- Tyuiu.BliznetsVG.Sprint0.Task3.V0.Lib/DataService.cs
+++ Tyuiu.BliznetsVG.Sprint0.Task3.V0.Lib/DataService.cs
@@ -8,6 +8,26 @@ namespace Tyuiu.BliznetsVG.Sprint0.Task3.V0.Lib
 {
     public class DataService
     {
+        public static int Addition(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int Division(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int Multiplication(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int Subtraction(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
         public static int Sum(int a, int b)
         {  return a + b; }
     }
